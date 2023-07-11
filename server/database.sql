9f1243b1f37d3ea28c273d65fb7be3cc3c986d3b
Grayce Muthui <graycemuthui20@gmail.com>
2023-07-11 14:52:28 +0300

    Add fk on table

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE reminder;
 
-CREATE TABLE users(
+CREATE TABLE students(
   id BIGINT PRIMARY KEY,
   first_name VARCHAR (255) NOT NULL,
   last_name VARCHAR (255) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE users(
 
 CREATE TABLE lecturers(
   id BIGINT PRIMARY KEY,
-  user_id BIGINT NOT NULL,
+  student_id BIGINT NOT NULL,
   first_name VARCHAR (255) NOT NULL,
   last_name VARCHAR (255) NOT NULL,
   email VARCHAR (255) NOT NULL,
@@ -21,66 +21,122 @@ CREATE TABLE lecturers(
   department VARCHAR (255) NOT NULL,
   phone_number BIGINT NOT NULL,
   date DATE
- CONSTRAINT fk_users
-    FOREIGN KEY(user_id)
-    REFERENCES users(id),
 );
+
+ALTER TABLE lecturers
+ADD CONSTRAINT fk_students
+    FOREIGN KEY(student_id)
+    REFERENCES students(id);
 
 CREATE TABLE courses(
   id BIGINT PRIMARY KEY,
-  user_id BIGINT NOT NULL,
+  student_id BIGINT NOT NULL,
   lecturer_id BIGINT NOT NULL,
   course_name VARCHAR (255) NOT NULL,
   course_code VARCHAR (255) NOT NULL,
   course_unit INT NOT NULL,
   date DATE
-CONSTRAINT fk_users
-    FOREIGN KEY(user_id)
-    REFERENCES users(id),
-CONSTRAINT fk_lecturers
-    FOREIGN KEY(lecturer_id)
-    REFERENCES lecturers(id),
 );
 
+ALTER TABLE courses
+ADD CONSTRAINT fk_students
+    FOREIGN KEY(student_id)
+    REFERENCES students(id);
 
+ALTER TABLE courses
+ADD CONSTRAINT fk_lecturers
+    FOREIGN KEY (lecturer_id)
+    REFERENCES lecturers(id);
 
 CREATE TABLE assignments(
   id BIGINT PRIMARY KEY,
-  user_id BIGINT NOT NULL,
-  lecturer_id INT NOT NULL,
-  assignment VARCHAR (255) NOT NULL,
+  student_id BIGINT NOT NULL,
+  lecturer_id BIGINT NOT NULL,
+  course_id BIGINT NOT NULL,
   date DATE
-CONSTRAINT fk_users
-    FOREIGN KEY(user_id)
-    REFERENCES users(id),
-CONSTRAINT fk_lecturers
-    FOREIGN KEY(lecturer_id)
-    REFERENCES lecturers(id),
 );
+
+ALTER TABLE assignments
+ADD CONSTRAINT fk_students
+    FOREIGN KEY(student_id)
+    REFERENCES students(id);
+
+ALTER TABLE assignments
+ADD CONSTRAINT fk_lecturers
+    FOREIGN KEY (lecturer_id)
+    REFERENCES lecturers(id);
+
+ALTER TABLE assignments
+ADD CONSTRAINT fk_courses
+    FOREIGN KEY (course_id)
+    REFERENCES courses(id);
+
+CREATE TABLE classes(
+  id BIGINT PRIMARY KEY,
+  student_id BIGINT NOT NULL,
+  lecturer_id BIGINT NOT NULL,
+  course_id BIGINT NOT NULL,
+  date DATE 
+);
+
+ALTER TABLE classes
+ADD CONSTRAINT fk_students
+    FOREIGN KEY(student_id)
+    REFERENCES students(id);
+
+ALTER TABLE classes
+ADD CONSTRAINT fk_lecturers
+    FOREIGN KEY (lecturer_id)
+    REFERENCES lecturers(id);
+
+ALTER TABLE classes
+ADD CONSTRAINT fk_courses
+    FOREIGN KEY (course_id)
+    REFERENCES courses(id);
 
 CREATE TABLE exams(
   id BIGINT PRIMARY KEY,
-  user_id BIGINT NOT NULL,
-  lecturer_id INT NOT NULL,
-  test VARCHAR (255) NOT NULL,
+  student_id BIGINT NOT NULL,
+  lecturer_id BIGINT NOT NULL,
+  course_id BIGINT NOT NULL,
   date DATE
-CONSTRAINT fk_users
-    FOREIGN KEY(user_id)
-    REFERENCES users(id),
-CONSTRAINT fk_lecturers
-    FOREIGN KEY(lecturer_id)
-    REFERENCES lecturers(id),
 );
-ss
 
+ALTER TABLE exams
+ADD CONSTRAINT fk_students
+    FOREIGN KEY(student_id)
+    REFERENCES students(id);
+
+ALTER TABLE exams
+ADD CONSTRAINT fk_lecturers
+    FOREIGN KEY (lecturer_id)
+    REFERENCES lecturers(id);
+
+ALTER TABLE exams
+ADD CONSTRAINT fk_courses
+    FOREIGN KEY (course_id)
+    REFERENCES courses(id);
 
 CREATE TABLE reminders(
   id BIGINT PRIMARY KEY,
-  user_id BIGINT NOT NULL,
+  student_id BIGINT NOT NULL,
   lecturer_id BIGINT NOT NULL,
+  course_id BIGINT NOT NULL,
   reminder VARCHAR (255) NOT NULL,
   date DATE
-CONSTRAINT fk_users
-    FOREIGN KEY(user_id)
-    REFERENCES users(id),
 );
+
+ALTER TABLE reminders
+ADD CONSTRAINT fk_students
+    FOREIGN KEY(student_id)
+    REFERENCES students(id);
+
+ALTER TABLE reminders
+ADD CONSTRAINT fk_lecturers
+    FOREIGN KEY (lecturer_id)
+    REFERENCES lecturers(id);
+
+ALTER TABLE reminders
+ADD CONSTRAINT fk_courses
+    FOREIGN KEY (course_id)
+    REFERENCES courses(id);
